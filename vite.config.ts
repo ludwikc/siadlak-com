66a1d04643e0afa8a6c16faf80acbef77924d44f
Ludwik C. Siadlak <ludwikc@users.noreply.github.com>
2026-03-28 14:59:20 +0100

    feat: add vite-imagetools plugin to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { imagetools } from "vite-imagetools";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    imagetools(),
     mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
